68529e4c05fb778f87fadde7fef24b06a130df8f
honaseung <lhs5948@gmail.com>
2023-01-27 02:36:55 +0900

    remove useless import

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,11 +1,5 @@
 import { Fragment, useState, useEffect } from "react";
 import Head from "next/head";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogActions from "@mui/material/DialogActions";
 import Typography from "@mui/material/Typography";
 import Link from "../components/Link";
 import { styled } from "@mui/material";
