1a586252a04f2bb32b98ac8f57bf4eedb2dd1e17
honaseung <lhs5948@gmail.com>
2023-01-27 21:18:34 +0900

    add name and phoneNumber input

--- renderer/pages/user/regist.tsx
+++ renderer/pages/user/regist.tsx
@@ -3,11 +3,17 @@ import { useState } from "react";
 import Link from "../../components/Link";
 import UserForm from "../../components/UserForm";
 import useCreateRequest from "../../lib/create-request";
-import { validateEmail, validatePasswod } from "../../lib/validate";
+import {
+  validateEmail,
+  validatePasswod,
+  validatePhoneNumber,
+} from "../../lib/validate";
 
 const Regist = () => {
   const [registId, setRegistId] = useState("");
+  const [registName, setRegistName] = useState("");
   const [registPassword, setRegistPassword] = useState("");
+  const [registPhoneNumber, setRegistPhoneNumber] = useState("");
   const [registPasswordConfirm, setRegistPasswordConfirm] = useState("");
 
   const router = useRouter();
@@ -22,6 +28,19 @@ const Regist = () => {
           setErr(false);
         }
         setRegistId(value);
+        break;
+
+      case "input-name":
+        setRegistName(value);
+        break;
+
+      case "input-number":
+        if (!validatePhoneNumber(value)) {
+          setErr(true);
+        } else {
+          setErr(false);
+        }
+        setRegistPhoneNumber(value);
         break;
 
       case "input-password":
@@ -53,7 +72,9 @@ const Regist = () => {
     "set",
     "",
     {
-      id: registId,
+      email: registId,
+      displayName: registName ? registName : registId,
+      phoneNumber: "+82" + registPhoneNumber.slice(1),
       password: registPassword,
     },
     [],
@@ -70,7 +91,8 @@ const Regist = () => {
         id={registId}
         password={registPassword}
         passwordConfirm={registPasswordConfirm}
-        // auth={auth}
+        number={registPhoneNumber}
+        name={registName}
         handleValue={handleValue}
         isRegist
         request={registRequest}
