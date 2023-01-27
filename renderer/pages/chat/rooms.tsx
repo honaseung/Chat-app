8670ea655c56b5662f512fb5783f3a6fd9764724
honaseung <lhs5948@gmail.com>
2023-01-28 02:12:09 +0900

    create rooms page

--- renderer/pages/chat/rooms.tsx
+++ renderer/pages/chat/rooms.tsx
@@ -1,44 +1,32 @@
-import { Button, TextareaAutosize } from "@mui/material";
+import Room from "../../components/Room";
 import { useState, useEffect } from "react";
-import Messages from "../../components/Mesaages";
-import { realtimeAddDoc, realtimeGetDocs } from "../../lib/firebaseAction";
+import { getUser, realtimeGetDocs } from "../../lib/firebaseAction";
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
+import { replaceAllSpecialChar } from "../../lib/utils";
 
 const Rooms = () => {
-  const getConversation = () => {
-    realtimeGetDocs(
-      { collectionType: "test/test" },
-      (response) => {
-        setConversation(response.val());
-      },
-      (error) => console.log(error)
-    );
-  };
+  const router = useRouter();
+  const user = getUser();
 
-  const addMessage = () => {
-    realtimeAddDoc(
-      {
-        collectionType: "test/test",
-        inputParams: {
-          ...conversation,
-          ["soma" + "-" + Date.now()]: "NICE3",
-        },
-      },
-      (response) => getConversation(),
-      (error) => console.log(error)
-    );
-  };
-
-  const [conversation, setConversation] = useState({});
+  const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
-    getConversation();
+    realtimeGetDocs(
+      {
+        collectionType: "chat",
+      },
+      (res) => setRooms(res.val()),
+      (err) => console.log(err)
+    );
   }, []);
-
   return (
     <>
-      <Messages conversation={conversation} />
-      <TextareaAutosize />
-      <Button onClick={addMessage}>보내기</Button>
+      {rooms &&
+        Object.keys(rooms).map((room, idx) => {
+          <Room key={idx} target={rooms[room]["+821094955948"]} />;
+        })}
+      <Button onClick={() => router.push("../user/users")}>Back</Button>
     </>
   );
 };
