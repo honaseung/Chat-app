3923dfc144ab4884f748ccc8a2cc7b270f1583d7
honaseung <lhs5948@gmail.com>
2023-01-27 21:19:40 +0900

    add phoneNumber validate

--- renderer/lib/validate.ts
+++ renderer/lib/validate.ts
@@ -1,9 +1,14 @@
 export function validateEmail(email) {
-  var regex =
+  const regex =
     /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
   return email && regex.test(email);
 }
 
 export function validatePasswod(password) {
   return 6 <= password.length && password.length <= 10;
+}
+
+export function validatePhoneNumber(number) {
+  const regPhoneNumber = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+  return number && regPhoneNumber.test(number);
 }
