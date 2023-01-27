0352ac69e30cd688549a05c530f2b3d7becc4cd4
honaseung <lhs5948@gmail.com>
2023-01-28 02:14:50 +0900

    create messages component

--- renderer/components/Mesaages.tsx
+++ renderer/components/Mesaages.tsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react";
+import { getUser } from "../lib/firebaseAction";
+import { replaceAllSpecialChar } from "../lib/utils";
+import { validateSameDay } from "../lib/validate";
 import Message from "./Message";
 
-const Messages = ({ conversation }) => {
-  console.log(Object.keys(conversation));
+const Messages = ({ userId, conversation }) => {
   return (
     <>
-      {conversation &&
-        Object.keys(conversation).map((key) => {
-          const [name, time] = key.split("-");
-          <Message
-            key={key}
-            name={name}
-            time={time}
-            message={conversation[key]}
-            mine
-          />;
-        })}
+      {Object.keys(conversation).map((key, idx, whole) => {
+        const [milliseconds, id, name] = key.split("-");
+        const prevDate = new Date(
+          parseInt(whole[idx - 1]?.split("-")[0] || "0", 10)
+        );
+        const date = new Date(parseInt(milliseconds, 10));
+        const sameDay = validateSameDay(date, prevDate);
+        return (
+          <>
+            {!sameDay ? (
+              <div key={idx} className="conversation-date">
+                {date.toLocaleDateString()}
+              </div>
+            ) : null}
+            <Message
+              key={key}
+              id={id}
+              name={name}
+              time={date.toLocaleTimeString()}
+              message={conversation[key]}
+              mine={replaceAllSpecialChar(userId, "_") === id}
+            />
+          </>
+        );
+      })}
     </>
   );
 };
