ecd0bd076fe74526958201ef8c4be6acbb7e053b
honaseung <lhs5948@gmail.com>
2023-01-28 20:47:18 +0900

    change from room component to html

--- renderer/pages/chat/rooms.tsx
+++ renderer/pages/chat/rooms.tsx
@@ -1,13 +1,13 @@
-import Room from "../../components/Room";
 import { useState, useEffect } from "react";
 import { getUser, realtimeGetDocs } from "../../lib/firebaseAction";
 import { Button } from "@mui/material";
 import { useRouter } from "next/router";
-import { replaceAllSpecialChar } from "../../lib/utils";
+import { createLocaleDateString, replaceAllSpecialChar } from "../../lib/utils";
 
 const Rooms = () => {
-  const router = useRouter();
   const user = getUser();
+
+  const router = useRouter();
 
   const [rooms, setRooms] = useState([]);
 
@@ -16,15 +16,46 @@ const Rooms = () => {
       {
         collectionType: "chat",
       },
-      (res) => setRooms(res.val()),
+      (response) => {
+        const res = response.val();
+        const list = Object.keys(res).filter((el) => {
+          if (el.includes(replaceAllSpecialChar(user.email, "_"))) {
+            return res[el];
+          }
+        });
+        setRooms(list);
+      },
       (err) => console.log(err)
     );
   }, []);
+  const enterRoom = (idx, room) => {
+    router.push({
+      pathname: "room",
+      query: { room },
+    });
+  };
   return (
     <>
       {rooms &&
-        Object.keys(rooms).map((room, idx) => {
-          <Room key={idx} target={rooms[room]["+821094955948"]} />;
+        rooms.map((room, idx) => {
+          return (
+            <>
+              참여자:
+              {room.split("-").map((title, idx) => {
+                if (idx == 0) return null;
+                return idx === 1 ? (
+                  <div key={idx}>
+                    {"생성 날짜: " + createLocaleDateString(title)}
+                  </div>
+                ) : (
+                  <div key={idx}>{title.split("_")[0]}</div>
+                );
+              })}
+              <Button key={idx + 1} onClick={() => enterRoom(idx, room)}>
+                들어가기
+              </Button>
+            </>
+          );
         })}
       <Button onClick={() => router.push("../user/users")}>Back</Button>
     </>
