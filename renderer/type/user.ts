349f12f8bd6736d8e7a35ecacaabc574e12b9074
honaseung <lhs5948@gmail.com>
2023-01-30 01:08:27 +0900

    change typescript type

--- renderer/type/user.ts
+++ renderer/type/user.ts
@@ -1,8 +1,15 @@
 export interface Iuser {
-  email?: string;
-  phoneNumber?: string;
+  email: string;
+  phoneNumber: string;
   displayName?: string;
-  id?: string;
-  password?: string;
-  changedId?: string;
+  id: string;
+  password: string;
 }
+
+export const defaultUser: Iuser = {
+  email: "",
+  phoneNumber: "",
+  displayName: "",
+  id: "",
+  password: "",
+};
