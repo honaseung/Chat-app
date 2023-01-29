236e8808d46e1a301d43333f4b271bb6ea3e5461
honaseung <lhs5948@gmail.com>
2023-01-30 01:09:37 +0900

    change javascript to typescript

--- renderer/pages/chat/rooms.tsx
+++ renderer/pages/chat/rooms.tsx
@@ -2,21 +2,18 @@ import { useState, useEffect } from "react";
 import { getUser, realtimeGetDocs } from "../../lib/firebaseApi";
 import {
   Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
 } from "@mui/material";
 import { useRouter } from "next/router";
-import { createLocaleDateString, replaceAllSpecialChar } from "../../lib/utils";
+import { replaceAllSpecialChar } from "../../lib/utils";
 import Loading from "../../components/Loading";
+import Room from "../../components/Room";
 
-const Rooms = () => {
+const Rooms: React.FunctionComponent = () => {
   const user = getUser();
 
   const router = useRouter();
 
-  const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState<string[]>([]);
 
   const [loading, setLoading] = useState(false);
 
@@ -26,31 +23,28 @@ const Rooms = () => {
       {
         collectionType: "chat",
       },
-      (response) => {
+      (response: any) => {
         setLoading(false);
         const res = response.val();
         if (res) {
-          const list = Object.keys(res).filter((el) => {
-            if (el.includes(replaceAllSpecialChar(user.email, "_"))) {
+          const list: string[] = Object.keys(res).filter((el) => {
+            if (el.includes(replaceAllSpecialChar(user?.email || '', "_"))) {
               return res[el];
             }
           });
           setRooms(list);
         }
       },
-      (error) => {
+      (error: any) => {
         setLoading(false);
         console.log(error);
       }
     );
   }, []);
-  const enterRoom = (room) => {
-    setLoading(true);
-    router.push({
-      pathname: "room",
-      query: { room },
-    });
-  };
+
+  if (loading) {
+    return null;
+  }
 
   return (
     <>
@@ -59,47 +53,13 @@ const Rooms = () => {
       ) : rooms.length > 0 ? (
         rooms.map((room, i) => {
           return (
-            <>
-              <Card sx={{ minWidth: 275, mt: 1, backgroundColor: "#d8ede7" }}>
-                <CardContent>
-                  {room.split("-").map((title, idx) => {
-                    if (idx == 0) return null;
-                    return idx === 1 ? (
-                      <>
-                        <Typography
-                          sx={{ fontSize: 14 }}
-                          color="text.secondary"
-                          gutterBottom
-                          key={title}
-                        >
-                          {"생성 날짜: " + createLocaleDateString(title)}
-                        </Typography>
-                        <Typography variant="h5" component="div" key={idx}>
-                          참여자:
-                        </Typography>
-                      </>
-                    ) : (
-                      <Typography
-                        sx={{ textAlign: "right" }}
-                        color="text.secondary"
-                        key={idx}
-                      >
-                        {title.split("_")[0]}
-                      </Typography>
-                    );
-                  })}
-                </CardContent>
-                <CardActions>
-                  <Button onClick={() => enterRoom(room)}>들어가기</Button>
-                </CardActions>
-              </Card>
-            </>
+            <Room room={room} key={i} />
           );
         })
       ) : (
         <div>참여중인 방이 없습니다.</div>
       )}
-      <Button onClick={() => router.push("../user/users")}>Back</Button>
+      <Button onClick={() => router.push("../user/users", undefined, { shallow: true })}>Back</Button>
     </>
   );
 };
